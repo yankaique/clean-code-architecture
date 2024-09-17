77dd50871a61b114e5106cdc22b6d15cf44f1cd7
yankaique <yan.contato.dev@gmail.com>
2024-09-16 21:22:52 -0300

    refact: aplicated code smells rules

--- src/signup.ts
+++ src/signup.ts
@@ -2,58 +2,48 @@ import crypto from "crypto";
 import pgp from "pg-promise";
 import { validateCpf } from "./validateCpf";
 
-export async function signup (input: any): Promise<any> {
+type TSignup = {
+	name: string;
+	email: string;
+	cpf: string;
+	carPlate: string;
+	isPassenger: boolean;
+	isDriver: boolean;
+}
+
+export async function signup (input: TSignup): Promise<any> {
 	const connection = pgp()("postgres://postgres:admin123@localhost:5432/postgres");
 	try {
+		const { name, email, cpf, carPlate, isDriver, isPassenger } = input
+		const [accountExist] = await connection.query("select * from cccat17.account where email = $1", [email]);
+		if (accountExist) return -4
+		const nameIsInvalid = !validateName(name);
+		if (nameIsInvalid) return -3;
+		const emailIsInvalid = !validateEmail(email);
+		if (emailIsInvalid) return -2;
+		const documentIsInvalid = !validateCpf(cpf);
+		if (documentIsInvalid) return -1;
+		const carPlateIsInvalid = !validatePlate(carPlate);
+		if (carPlateIsInvalid) return -5;
 		const id = crypto.randomUUID();
-
-		const [acc] = await connection.query("select * from cccat17.account where email = $1", [input.email]);
-		if (!acc) {
-
-			if (input.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
-				if (input.email.match(/^(.+)@(.+)$/)) {
-
-					if (validateCpf(input.cpf)) {
-						if (input.isDriver) {
-							if (input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
-								await connection.query("insert into cccat17.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver]);
-								
-								const obj = {
-									accountId: id
-								};
-								return obj;
-							} else {
-								// invalid car plate
-								return -5;
-							}
-						} else {
-							await connection.query("insert into cccat17.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver]);
-
-							const obj = {
-								accountId: id
-							};
-							return obj;
-						}
-					} else {
-						// invalid cpf
-						return -1;
-					}
-				} else {
-					// invalid email
-					return -2;
-				}
-
-			} else {
-				// invalid name
-				return -3;
-			}
-
-		} else {
-			// already exists
-			return -4;
-		}
-
+		await connection.query("insert into cccat17.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, name, email, cpf, carPlate, isPassenger, isDriver]);
+		const userInformation = {
+			accountId: id
+		};
+		return userInformation;
 	} finally {
 		await connection.$pool.end();
 	}
+}
+
+function validateName(name: string) {
+	return name.match(/[a-zA-Z] [a-zA-Z]+/);
+}
+
+function validateEmail(email: string) {
+	return email.match(/^(.+)@(.+)$/);
+}
+
+function validatePlate(plate: string) {
+	return plate.match(/[A-Z]{3}[0-9]{4}/);
 }
