2bac15f7c51276b6bb9d3a2089ce4155d6029809
yankaique <yan.contato.dev@gmail.com>
2024-09-17 16:39:30 -0300

    fix: fixing magic numbers rule

--- src/signup.ts
+++ src/signup.ts
@@ -11,20 +11,28 @@ type TSignup = {
 	isDriver: boolean;
 }
 
+enum errorCode {
+	accountAlreadyExist = -4,
+	nameInvalid = -3,
+	emailInvalid = -2,
+	documentInvalid = -1,
+	carPlateInvalid =  -5
+}
+
 export async function signup (input: TSignup): Promise<any> {
 	const connection = pgp()("postgres://postgres:admin123@localhost:5432/postgres");
 	try {
 		const { name, email, cpf, carPlate, isDriver, isPassenger } = input
 		const [accountExist] = await connection.query("select * from cccat17.account where email = $1", [email]);
-		if (accountExist) return -4
+		if (accountExist) return errorCode.accountAlreadyExist;
 		const nameIsInvalid = !validateName(name);
-		if (nameIsInvalid) return -3;
+		if (nameIsInvalid) return errorCode.nameInvalid;
 		const emailIsInvalid = !validateEmail(email);
-		if (emailIsInvalid) return -2;
+		if (emailIsInvalid) return errorCode.emailInvalid;
 		const documentIsInvalid = !validateCpf(cpf);
-		if (documentIsInvalid) return -1;
+		if (documentIsInvalid) return errorCode.documentInvalid;
 		const carPlateIsInvalid = !validatePlate(carPlate);
-		if (carPlateIsInvalid) return -5;
+		if (carPlateIsInvalid) return errorCode.carPlateInvalid;
 		const id = crypto.randomUUID();
 		await connection.query("insert into cccat17.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, name, email, cpf, carPlate, isPassenger, isDriver]);
 		const userInformation = {
